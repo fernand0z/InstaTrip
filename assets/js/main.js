16e877555d312e51d1f8441ed83a3f8f74827bef
Fernando <fernandoz@outlook.com>
2018-07-17 21:25:45 -0700

    Cloned repo commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// $( document ).ready(function(){
+//     $(".button-collapse").sideNav();
+
+//     $('.carousel.carousel-slider').carousel({fullWidth: true});
+
+//     setInterval(function(){
+//         $('.carousel').carousel('next');
+//     }, 4000);
+
+
+
+// });
